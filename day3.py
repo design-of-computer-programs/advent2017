29e7437c196c425c544b93cdcb1c412ea48fffb2
fortymiles <mqgao@outlook.com>
2017-12-16 19:04:26 +0800

    fix day3 structure

--- day3.py
+++ day3.py
@@ -7,15 +7,14 @@ def generate_sequence(N, initial_position=(0, 0), initial_value=1):
     already_position = {}
     next_step = initial_position
     for i in range(N):
-        if i != 0: 
-            next_step = generate_next_step(*position, already_position=already_position)
+        if i != 0:  next_step = generate_next_step(*position, already_position=already_position)
 
-        if i == 0:
+        if i == 0: 
             position_value = initial_value
         else:
             arounds_steps = get_around_positions(*next_step)
-            #position_value = sum(already_position.get(p, 0) for p in arounds_steps)
-            position_value = i + 1
+            position_value = sum(already_position.get(p, 0) for p in arounds_steps)
+            #position_value = i + 1
 
         already_position[next_step] = position_value
         position = next_step
@@ -35,18 +34,12 @@ def get_around_positions(x, y):
 
 
 def generate_next_step(x, y, already_position=set()): 
-    if y == 0 and x > 0: step = x, y + 1
-    elif y == 0 and x < 0: step = x, y - 1
-    elif x == 0 and y > 0: step = x - 1, y
-    elif x == 0 and y < 0: step = x + 1, y
-    else:
-        next_possible_step = ((x+1, y), (x, y+1), (x-1, y), (x, y-1))
-        next_possible_step = [step for step in next_possible_step if step not in already_position]
-        def distance(x, y): return (x + y) * (x + y) - 2 * x * y
-        min_distance = min(map(lambda step: distance(*step), next_possible_step))
-        for step in next_possible_step: 
-            if distance(*step) == min_distance: 
-                break
+    next_possible_step = ((x+1, y), (x, y+1), (x-1, y), (x, y-1))
+    next_possible_step = [step for step in next_possible_step if step not in already_position]
+
+    def distance(x, y): return (x + y) * (x + y) - 2 * x * y
+
+    step = min(map(lambda step: (distance(*step), step), next_possible_step), key=lambda x: x[0])[1]
 
     return step
 
@@ -71,14 +64,14 @@ assert sorted(get_around_positions(0, 0)) == sorted([(1, 0), (1, 1), (0, 1), (-1
 assert sorted(get_around_positions(-1, -1)) == sorted([(0, 0), (-1, 0), (-2, 0), (-2, -1), (-2, -2), (-1, -2), (0, -2), (0, -1)])
 
 N = 361527
-for step, position in generate_sequence(N):
+#for step, position in generate_sequence(N):
 #    if step % 1000 == 0: 
 #        print(step, position)
-    if step == N: 
-        print(abs(position[0]) + abs(position[1]))
+#    if step == N: 
+#        print(abs(position[0]) + abs(position[1]))
 
-#for value, position in generate_sequence(N):
-#    if value <= N: 
-#        print(value)
-#    else:
-#        break
+# question 2
+for value, position in generate_sequence(N):
+    if value > N: 
+        print(value)
+        break
